858918551bf7c9f16d949be7502714b89c803f3d
vaibhavwxyz <vaibhawxyz@gmail.com>
2023-06-02 21:28:42 +0530

    final commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="fixed bottom-0 right-0 left-0 bg-slate-800 text-white">
+      <div className="container mx-auto max-w-5xl py-2">
+        <p className="text-center font-bold">All rights reserved ©</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
